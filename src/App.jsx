0d92a89fe20067cc6776f88bc3803391b142a061
Dibyendu-13 <dibyendubar13702@gmail.com>
2026-01-25 13:29:37 +0530

    minor-changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import NavBar from './components/Navbar'
+import NavBar from './components/NavBar'
 import Hero from './components/Hero'
 import ProductViewer from './components/ProductViewer'
 import gsap from 'gsap'
